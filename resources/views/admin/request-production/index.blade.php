f8a9dbd46a6e6457db4e75854aced5d3f762a112
iqbalatma <iqbalatma@gmail.com>
2023-07-18 00:14:02 +0700

    add empty data component

--- resources/views/admin/request-production/index.blade.php
+++ resources/views/admin/request-production/index.blade.php
@@ -7,68 +7,73 @@
 					<h4 class="card-title">{{$cardTitle ?? "Produk"}}</h4>
 				</div>
 				<div class="card-body pt-0">
-					<div class="pt-4">
-						<div class="table-responsive">
-							<form action="{{route('admin.request.production.update')}}" method="POST">
-								@csrf
-								@method('PATCH')
-								@if(request()->query("status", "waiting") === "waiting")
-								<div class="d-grid gap-2 d-md-block">
-									<button class="btn btn-info" type="reset">Reset</button>
-									<button class="btn btn-primary" type="submit">Simpan</button>
-								</div>
-								@endif
-								<table class="table">
-									<thead>
-									<tr>
-										@if(request()->query("status") === "waiting")
-											<th scope="col">Aksi</th>
-										@endif
-										<th scope="col">Nama Produk</th>
-										<th scope="col">Kuantitas Gudang</th>
-										<th scope="col">Ambang Batas Kuantitas</th>
-										<th scope="col">Permintaan Produksi</th>
-										<th scope="col">Berat (gram)</th>
-										<th scope="col">Status</th>
-									</tr>
-									</thead>
-									<tbody>
-									@foreach($requestProductions as $key => $requestProduction)
+					@if($requestProductions->count() === 0)
+						<x-admin.empty-data></x-admin.empty-data>
+					@else
+						<div class="pt-4">
+							<div class="table-responsive">
+								<form action="{{route('admin.request.production.update')}}" method="POST">
+									@csrf
+									@method('PATCH')
+									@if(request()->query("status", "waiting") === "waiting")
+										<div class="d-grid gap-2 d-md-block">
+											<button class="btn btn-info" type="reset">Reset</button>
+											<button class="btn btn-primary" type="submit">Simpan</button>
+										</div>
+									@endif
+									<table class="table">
+										<thead>
 										<tr>
 											@if(request()->query("status") === "waiting")
-												<td>
-													<div class="d-grid gap-2 d-md-block">
-														<input class="form-check-input request-production-id"
-														       type="checkbox" name="request_production_ids[]"
-														       value="{{$requestProduction->id}}">
-														<input type="number"
-														       class="form-control-sm d-none request-production-quantity"
-														       name="actual_quantities[]" disabled>
-													</div>
-												</td>
+												<th scope="col">Aksi</th>
 											@endif
-
-											<td>{{$requestProduction->product?->name??"-"}}</td>
-											<td>{{($requestProduction->product?->quantity??0) . " pcs"}}</td>
-											<td>{{($requestProduction->product?->quantity_threshold??0) . " pcs"}}</td>
-											<td>{{ ($requestProduction->request_quantity) . " pcs"}}</td>
-											<td>{{ ($requestProduction->product?->weight??0) . " grams"}}</td>
-											<td>
-												@if($requestProduction->status === "waiting")
-													<span class="badge bg-warning">Menunggu</span>
-												@elseif($requestProduction->status === "done")
-													<span class="badge bg-success">Selesai</span>
-												@else
-													<span class="badge bg-danger">Batal</span>
-												@endif
-											</td>
+											<th scope="col">Nama Produk</th>
+											<th scope="col">Kuantitas Gudang</th>
+											<th scope="col">Ambang Batas Kuantitas</th>
+											<th scope="col">Permintaan Produksi</th>
+											<th scope="col">Berat (gram)</th>
+											<th scope="col">Status</th>
 										</tr>
-									@endforeach
-									</tbody>
-								</table>
-							</form>
+										</thead>
+										<tbody>
+										@foreach($requestProductions as $key => $requestProduction)
+											<tr>
+												@if(request()->query("status") === "waiting")
+													<td>
+														<div class="d-grid gap-2 d-md-block">
+															<input class="form-check-input request-production-id"
+															       type="checkbox" name="request_production_ids[]"
+															       value="{{$requestProduction->id}}">
+															<input type="number"
+															       class="form-control-sm d-none request-production-quantity"
+															       name="actual_quantities[]" disabled>
+														</div>
+													</td>
+												@endif
+
+												<td>{{$requestProduction->product?->name??"-"}}</td>
+												<td>{{($requestProduction->product?->quantity??0) . " pcs"}}</td>
+												<td>{{($requestProduction->product?->quantity_threshold??0) . " pcs"}}</td>
+												<td>{{ ($requestProduction->request_quantity) . " pcs"}}</td>
+												<td>{{ ($requestProduction->product?->weight??0) . " grams"}}</td>
+												<td>
+													@if($requestProduction->status === "waiting")
+														<span class="badge bg-warning">Menunggu</span>
+													@elseif($requestProduction->status === "done")
+														<span class="badge bg-success">Selesai</span>
+													@else
+														<span class="badge bg-danger">Batal</span>
+													@endif
+												</td>
+											</tr>
+										@endforeach
+										</tbody>
+									</table>
+								</form>
+							</div>
 						</div>
-					</div>
+					@endif
+
 				</div>
 			</div>
 		</div>
---
+++ resources/views/components/admin/empty-data.blade.php
@@ -0,0 +1,6 @@
+<div class="card text-center">
+    <div class="card-body">
+        <h5 class="card-title">Data Tidak Tersedia</h5>
+        <p class="card-text">Data yang anda minta tidak tersedia.</p>
+    </div>
+</div>
